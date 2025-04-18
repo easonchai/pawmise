9765d7dc1d26fe53cbe9d342f04df88f22c39301
mengo <menghong6988@gmail.com>
2025-04-18 14:16:29 +0800

    Feat: Test goat-sdk init

---
+++ ai/src/index.ts
@@ -0,0 +1,72 @@
+import readline from "node:readline";
+
+import { openai } from "@ai-sdk/openai";
+import { generateText } from "ai";
+
+import { getOnChainTools } from "@goat-sdk/adapter-vercel-ai";
+import { PEPE, USDC, erc20 } from "@goat-sdk/plugin-erc20";
+
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+// 1. Create a wallet client
+// const account = privateKeyToAccount(
+//   process.env.WALLET_PRIVATE_KEY as `0x${string}`,
+// );
+
+// const walletClient = createWalletClient({
+//   account: account,
+//   transport: http(process.env.RPC_PROVIDER_URL),
+//   chain: baseSepolia,
+// });
+
+(async () => {
+  // 2. Get your onchain tools for your wallet
+  // const tools = await getOnChainTools({
+  //   wallet: viem(walletClient),
+  //   plugins: [
+  //     sendETH(), // Enable ETH transfers
+  //     erc20({ tokens: [USDC, PEPE] }), // Enable ERC20 token operations
+  //   ],
+  // });
+
+  // 3. Create a readline interface to interact with the agent
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  while (true) {
+    const prompt = await new Promise<string>((resolve) => {
+      rl.question('Enter your prompt (or "exit" to quit): ', resolve);
+    });
+
+    if (prompt === "exit") {
+      rl.close();
+      break;
+    }
+
+    console.log("\n-------------------\n");
+    console.log("TOOLS CALLED");
+    console.log("\n-------------------\n");
+    try {
+      const result = await generateText({
+        model: openai("gpt-4o-mini"),
+        // tools: tools,
+        maxSteps: 10, // Maximum number of tool invocations per request
+        prompt: prompt,
+        onStepFinish: (event) => {
+          console.log(event.toolResults);
+        },
+      });
+
+      console.log("\n-------------------\n");
+      console.log("RESPONSE");
+      console.log("\n-------------------\n");
+      console.log(result.text);
+    } catch (error) {
+      console.error(error);
+    }
+    console.log("\n-------------------\n");
+  }
+})();
