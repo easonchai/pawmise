1d9b5a9a90b3d60affd582c52794a7757294a3c4
amadzai <20966134+amadzai@users.noreply.github.com>
2025-04-19 20:47:23 +0800

    feat: Add get pet route

--- backend/src/pet/pet.controller.ts
+++ backend/src/pet/pet.controller.ts
@@ -1,6 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, NotFoundException } from '@nestjs/common';
+import { PetService } from './pet.service';
 
 @Controller('pet')
 export class PetController {
-  
+  constructor(private readonly petService: PetService) {}
+
+  @Get(':id')
+  async getPetInfo(@Param('id') id: string) {
+    const petInfo = await this.petService.getPet({
+      where: { id },
+      include: {
+        user: true,
+      },
+    });
+
+    if (!petInfo) {
+      throw new NotFoundException('Pet not found');
+    }
+
+    return petInfo;
+  }
 }
